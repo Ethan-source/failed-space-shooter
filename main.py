5d92c939b3d70a98a94e6b67b1e49460dc490e8d
Yee <yeeet@iu.edu>
2019-10-02 16:48:33 -0400

    completing the assignment

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = ""
 
+NUM_ENEMIES = 5
+STARTING_LOCATION = (400, 300)
+BULLET_DAMAGE= 10
+ENEMY_HP = 20
+HIT_SCORE = 10
+KILL_SCORE = 100
+
 
 class Window(arcade.Window):
 
@@ -29,35 +36,47 @@ class Window(arcade.Window):
 
 
     def setup(self):
+        self.center_x += self.dx
+        self.center_y += self.dy
         pass 
-
-    def update(self, delta_time):
+   
+ 
+            
+def update(self, delta_time):
         pass
 
-    def on_draw(self):
+def on_draw(self):
         """ Called whenever we need to draw the window. """
         arcade.start_render()
 
 
 
 
-    def on_mouse_motion(self, x, y, dx, dy):
+def on_mouse_motion(self, x, y, dx, dy):
         """ Called to update our objects. Happens approximately 60 times per second."""
+        self.ball.position_x = x #has the mouse follow the ball on the x axis
+        self.ball.position_y = y #has the mouse follow the ball on the Y axis
         pass
 
-    def on_mouse_press(self, x, y, button, modifiers):
+def on_mouse_press(self, x, y, button, modifiers):
         """
         Called when the user presses a mouse button.
         """
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            x= self.player.center_x
+            y= self.player.center_y + 15
+            bullet = Bullet ((x,y),(0,10),BULLET_DAMAGE)
+            self.bullet_list.append(bullet)
+
         pass
 
-    def on_mouse_release(self, x, y, button, modifiers):
+def on_mouse_release(self, x, y, button, modifiers):
         """
         Called when a user releases a mouse button.
         """
         pass
 
-    def on_key_press(self, key, modifiers):
+def on_key_press(self, key, modifiers):
         """ Called whenever the user presses a key. """
         if key == arcade.key.LEFT:
             print("Left")
@@ -68,7 +87,7 @@ class Window(arcade.Window):
         elif key == arcade.key.DOWN:
             print("Down")
 
-    def on_key_release(self, key, modifiers):
+def on_key_release(self, key, modifiers):
         """ Called whenever a user releases a key. """
         pass
 
